c96f0f5d151bddb4882c55697f2a49faa028b99d
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2026-04-16 14:08:39 +0100

    migrations/001_init.sql

---
+++ backend/migrations/001_init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    count integer DEFAULT 0,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE IF NOT EXISTS public.saves
+(
+    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    group_id character varying(20) COLLATE pg_catalog."default",
+    value integer,
+    "timestamp" bigint,
+    CONSTRAINT saves_pkey PRIMARY KEY (id),
+    CONSTRAINT saves_group_id_fkey FOREIGN KEY (group_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
